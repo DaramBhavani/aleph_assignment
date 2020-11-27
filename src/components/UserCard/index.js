45a893871e00c18b751157465b259fb8e24b1e5f
daram.bhavani@gmail.com <daram.bhavani@gmail.com>
2020-11-27 23:45:52 +0800

    initial commit of UserCard Component

---
+++ src/components/UserCard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { IconButton } from "../index";
+
+const UserCard = () => {
+  return (
+    <div>
+      <p>UserCard Component</p>
+      <IconButton />
+    </div>
+  );
+};
+
+export default UserCard;
