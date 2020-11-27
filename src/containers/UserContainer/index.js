68099b56007138358f4203222ef40372743b76b6
daram.bhavani@gmail.com <daram.bhavani@gmail.com>
2020-11-27 23:51:31 +0800

    initial commit of UserContainer Component

---
+++ src/containers/UserContainer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { UserCard, Search } from "../../components/index";
+
+const UserContainer = () => {
+  return (
+    <div>
+      <p>UserContainer</p>
+      <Search />
+      <UserCard />
+    </div>
+  );
+};
+
+export default UserContainer;
