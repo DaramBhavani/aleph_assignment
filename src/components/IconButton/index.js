ccf749f13bbfedb30843f2c86d4fae28f6ef77c3
daram.bhavani@gmail.com <daram.bhavani@gmail.com>
2020-11-27 23:40:27 +0800

    initial commit of IconButton Component

---
+++ src/components/IconButton/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IconButton = () => {
+  return (
+    <div>
+      <p>IconButton Component</p>
+      <button>Contact Now</button>
+    </div>
+  );
+};
+
+export default IconButton;
