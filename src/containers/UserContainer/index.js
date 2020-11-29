49b8d87b435344c3e4335967be7ecf2aa9229d49
daram.bhavani@gmail.com <daram.bhavani@gmail.com>
2020-11-29 14:47:06 +0800

    API integration and implementation with css

--- src/containers/UserContainer/index.js
+++ src/containers/UserContainer/index.js
@@ -1,13 +1,35 @@
-import React from "react";
-import { UserCard, Search } from "../../components/index";
+import React, { useState, useEffect } from "react";
+import { UserCard } from "../../components/index";
+import getUsers from "../../api/index";
+import Styled from "styled-components";
+
+const StyleUserContainer = Styled.div`
+display:flex;
+flex-wrap:wrap;
+justify-content:center;
+`;
 
 const UserContainer = () => {
+  const [userDetails, setUserDetails] = useState([]);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const result = await getUsers();
+      console.log(result.data);
+      setUserDetails(result.data);
+    };
+    loadUsers();
+  }, []);
+
+  console.log("userDetails:", userDetails);
+
   return (
-    <div>
-      <p>UserContainer</p>
-      <Search />
-      <UserCard />
-    </div>
+    <StyleUserContainer>
+      {userDetails.map((user) => {
+        console.log(user);
+        return <UserCard {...user} key={user.id} />;
+      })}
+    </StyleUserContainer>
   );
 };
 
