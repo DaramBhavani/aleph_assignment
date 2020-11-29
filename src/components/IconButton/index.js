cb3dab37e830f2e04d67dfe651aa2fbe80971e4d
daram.bhavani@gmail.com <daram.bhavani@gmail.com>
2020-11-29 14:29:24 +0800

    created button and applied styles

--- src/components/IconButton/index.js
+++ src/components/IconButton/index.js
@@ -1,10 +1,30 @@
 import React from "react";
+import Styled from "styled-components";
+
+const StyleButton = Styled.button`
+border:1px solid black;
+float:right;
+margin-right:1rem;
+margin-bottom:1rem;
+background-color: #5643fa;
+border-radius:1.5rem;
+color:#fff;
+padding:.5rem;
+font-size:1.10rem;
+
+span{
+  margin-right:5px;
+  align-self:center;
+}
+`;
 
 const IconButton = () => {
   return (
     <div>
-      <p>IconButton Component</p>
-      <button>Contact Now</button>
+      <StyleButton>
+        <span>&#9990;</span>
+        <span>Contact Now</span>
+      </StyleButton>
     </div>
   );
 };
