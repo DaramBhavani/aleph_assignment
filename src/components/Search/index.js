b3699257f23591ab9a178b9ccedd3bec7603ba0e
daram.bhavani@gmail.com <daram.bhavani@gmail.com>
2020-11-30 10:57:52 +0800

    implementation of search component with css

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,10 +1,33 @@
 import React from "react";
+import Styled from "styled-components";
 
-const Search = () => {
+const StyleSearch = Styled.div`
+display:flex;
+justify-content:center;
+*:focus{
+  outline:none;
+}
+input{
+  flex:0 0 30%;
+  border:1px solid black;
+  border-radius:1.5rem;
+  box-shadow:0.5px 1px 4px rgba(0, 0, 0, 0.4);
+  padding:.7rem 2rem;
+  margin:1rem; 
+}
+`;
+
+const Search = (props) => {
   return (
-    <div>
-      <p>Search Component</p>
-    </div>
+    <StyleSearch>
+      <input
+        type="text"
+        placeholder="enter your name/email"
+        value={props.text}
+        onChange={(event) => props.onLoadUserData(event.target.value)}
+      ></input>
+      <span>&#128269;</span>
+    </StyleSearch>
   );
 };
 
